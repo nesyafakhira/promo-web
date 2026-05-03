a06552b1df91a22e1d23ba8fcdcd7bce54aa0b64
nesyafakhira <nesyafkhrarianti75@gmail.com>
2026-05-03 08:48:31 +0700

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#8A151B',
+        accent: '#E6C287',
+        bglight: '#F9F6F0',
+        textdark: '#2D2D2D',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
